0b61bb6b4a5481946df4e27f4a4e7cd16aa12b66
fateme marashi <88906546+ftmemarashi@users.noreply.github.com>
2023-06-06 13:56:37 +0330

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Component } from 'react'
+import Login from './Components/Login'
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        
+    <Login/>
+      </div>
+    )
+  }
+}
